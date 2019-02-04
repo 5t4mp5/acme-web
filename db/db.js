3843ce160c564e16b1d6b4ce42133aac22c7edbe
Admin <admin@Admins-MBP.home>
2019-02-03 21:53:27 -0500

    data layer written

---
+++ db/db.js
@@ -0,0 +1,20 @@
+//methods to get back user and users
+
+const pg = require('pg');
+const client = new pg.Client('postgress://localhost/acme_company');
+
+function getUsers(){
+  return client.query('SELECT * FROM officers')
+    .then(response => response.rows);
+}
+
+function getUser(id){
+  return client.query('SELECT * FROM officers WHERE id = $1', [id])
+    .then(response => response.rows[0]);
+}
+
+client.connect()
+  .then(() => console.log('connected'))
+  .catch(err => console.log(err));
+
+module.exports = {getUser, getUsers};
