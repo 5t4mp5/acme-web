dcf7250993aa8d4d8bad2d8a38aff9d8a3edfa4b
Admin <admin@Admins-MBP.home>
2019-02-03 22:57:33 -0500

    seperated server, db, and app file

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-const db = require('./db/db');
+const express = require('express');
+const app = express();
+const db = require('./db');
 
+app.use((req, res, next) => {
+  db.getUsers()
+    .then(users => {
+      req.users = users;
+      next();
+    })
+    .catch(next);
+});
+
+app.get('/', (req, res, next) => {
+  res.send(req.users);
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,7 @@
+const db = require('./db');
+const app = require('./app');
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => console.log(`Listening on Port ${port}`));
+
